82e21ba2090c962eb32bb16adc68a58811c3c3d5
Muhammad Attique Anwar <muhammad.attique@falconi.local>
2023-08-29 19:01:06 +0500

    Implemented basic store using actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {addToCart} from "./redux/action"
+import {useDispatch} from "react-redux"
 function App() {
+  const dispatch = useDispatch();
+ const product = {
+    name: "Paste",
+    price: 100,
+    type: "household",
+    color: "white"
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> dispatch(addToCart(product))}>Add to Cart</button>
     </div>
   );
 }
