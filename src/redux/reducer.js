e533b2ef40fa81571795aeb1bbad7107d5003b62
Muhammad Attique Anwar <muhammad.attique@falconi.local>
2023-08-31 12:48:47 +0500

    Implemented redux in component

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,9 +3,9 @@ import { ADD_TO_CART } from "./constants";
 export const cartData = (data = [] , action) =>{
     if (action.type === ADD_TO_CART){
         console.log("Reducer called", action)
-        return data;
+        return [action.data, ...data];
     }
     else {
-        return "No action called";
+        return data;
     }
 }
