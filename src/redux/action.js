8e0aa557e7380783aef993556b77cfa2d2dc0c36
Muhammad Attique Anwar <muhammad.attique@falconi.local>
2023-08-31 13:46:17 +0500

    Implemented remove and empty cart

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import {ADD_TO_CART} from './constants'
+import {ADD_TO_CART, EMPTY_CART, REMOVE_FROM_CART} from './constants'
 
 export const addToCart = (data) => {
     console.log ("action called", data);
@@ -6,4 +6,17 @@ return {
     type: ADD_TO_CART,
     data: data
 }
+}
+export const removeFromCart = (data) => {
+    console.log ("item removed from cart", data);
+    return{
+    type: REMOVE_FROM_CART,
+    data: data
+    }
+}
+export const emptyCart = () => {
+    console.log ("empty cart");
+    return{
+    type: EMPTY_CART,
+    }
 }
